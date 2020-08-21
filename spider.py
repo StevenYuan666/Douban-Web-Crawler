fd7f085b18bce878da25fe28d29ee3184bf386b3
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2020-08-21 14:45:38 +0800

    create a new file and import related modules

---
+++ spider.py
@@ -0,0 +1,18 @@
+#-*- coding = utf-8 -*-
+#@auhotr:Steven Yuan
+
+#import the needed module
+#analyze the website
+import bs4
+#expression, text matching
+import re
+#input URL, get website data
+import urllib
+#operate Excel
+import xlwt
+#operate SQLite
+import sqlite3
+
+
+if __name__ == '__main__':
+    print('666')
